839a52484bdd226726de0a8aaa92e5a5e6a232b1
github username <github email address>
2018-08-29 17:45:23 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested div div div .target')
+}
+
+function deepestChild(){
+  return document.querySelector('#grand-node div div div div')
+}
+
+function increaseRankBy(n){
+  let rankList = document.querySelectorAll('.ranked-list')
+  for (let i = 0, l = rankList.length; i < l; i++) {
+    let children = rankList[i].children;
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n;
+    }
+  }
+}
